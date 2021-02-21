31505ee834ecbb6003e59f55ba4f2ae78bfb0b06
amroshworld <amrosh.world@gmail.com>
2021-02-21 21:32:16 +0000

    update

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
 function compute()
 {
    var principal = document.getElementById("principal").value;
-   var rate = document.getElementById("rate").value;
    var years = document.getElementById("years").value;
-   var interest = principal * rate / 100 ;
+   var rate = document.getElementById("inter").value;
+   var interest = principal * rate / 10 ;
    var futureyear = parseInt(years) + 2021;
    
-   document.getElementById("result").innerHTML = `If you deposit <mark>${principal}</mark>. <br> at an interest rate of <mark>${rate}</mark>.<br> You will receive an amount of <mark>${interest}</mark>,<br> in the year <mark>${futureyear}</mark>`;
-
-   if(principal == 0 || ""){
-     alert("enter valid number");
+   if(principal == 0 || principal == "" || principal < 0){
+     alert("enter a positive number");
+     document.getElementById("principal").focus();
+     return;
    }
   
+   document.getElementById("result").innerHTML = `If you deposit <mark>${principal}</mark>. <br> at an interest rate of <mark>${rate}</mark>.<br> You will receive an amount of <mark>${interest}</mark>,<br> in the year <mark>${futureyear}</mark>`;
+
     
 }
    var slider = document.getElementById("inter");
    var output = document.getElementById("demo");
    slider.oninput = function() {
-   output.innerHTML = this.value;
+   output.innerHTML = this.value + "%";
 }
          
